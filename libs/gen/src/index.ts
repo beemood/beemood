f65d38d77eef877544600fbcf6b29c6ad02acd1e
Robert Brightline <robert.brightline@gmail.com>
2025-04-06 08:42:52 -0500

    imp: configured versions

--- libs/gen/src/index.ts
+++ libs/gen/src/index.ts
@@ -3,6 +3,8 @@ export * from './api/api.js';
 export * from './api/schema.d.js';
 export * from './cli/cli.js';
 export * from './cli/schema.d.js';
+export * from './generator/generator.js';
+export * from './generator/schema.d.js';
 export * from './lib/lib.js';
 export * from './lib/schema.d.js';
 
