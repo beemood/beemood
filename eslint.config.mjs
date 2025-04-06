980e3856e4f5e15ab6ec520174bbb335f1d01700
Robert Brightline <robert.brightline@gmail.com>
2025-04-06 12:51:55 -0500

    feat: linter update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,3 @@
 import baseLint from './.eslint/eslint.config.mjs';
 
-export default [
-  {
-    files: ['**/*.json'],
-    // Override or add rules here
-    rules: {},
-    languageOptions: {
-      parser: await import('jsonc-eslint-parser'),
-    },
-  },
-  ...baseLint,
-  {
-    ignores: ['**/vite.config.*.timestamp*', '**/vitest.config.*.timestamp*'],
-  },
-];
+export default [...baseLint];
