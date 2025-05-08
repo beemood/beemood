e25c703e0b55c79d15018c90f31a561553eca79e
Robert Brightline <robert.brightline@gmail.com>
2025-05-08 12:43:09 -0500

    gen:created project generator and configured

--- libs/gen/src/index.ts
+++ libs/gen/src/index.ts
@@ -1,6 +1,4 @@
-// @index(['./**/*.ts', '!./**/*.{spec,test}.ts'], f => `export * from '${f.path}.js'`)
+// @index(['./**/*.ts', '!./**/*.{spec,test,d}.ts'], f => `export * from '${f.path}.js'`)
 export * from './generators/project/project.js';
-export * from './generators/project/schema.d.js';
 export * from './generators/resource/resource.js';
-export * from './generators/resource/schema.d.js';
 export * from './lib/gen.js';
