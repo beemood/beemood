18a5a4c10b29543134932e19d7733514136a7f9f
Robert Brightline <robert.brightline@gmail.com>
2025-05-24 13:27:21 -0500

    published

--- apps/beemood/eslint.config.mjs
+++ apps/beemood/eslint.config.mjs
@@ -1,14 +1,3 @@
 import baseConfig from '../../eslint.config.mjs';
 
-export default [
-  ...baseConfig,
-  {
-    files: ['**/package.json', '**/generators.json'],
-    rules: {
-      '@nx/nx-plugin-checks': 'error',
-    },
-    languageOptions: {
-      parser: await import('jsonc-eslint-parser'),
-    },
-  },
-];
+export default [...baseConfig];
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -46,4 +46,13 @@ export default [
       ],
     },
   },
+  {
+    files: ['**/package.json', '**/generators.json'],
+    rules: {
+      '@nx/nx-plugin-checks': 'error',
+    },
+    languageOptions: {
+      parser: await import('jsonc-eslint-parser'),
+    },
+  },
 ];
