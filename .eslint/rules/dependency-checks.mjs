ce8b7409c01e28d7a3ba2bcc4eceeca644d8646d
Robert Brightline <robert.brightline@gmail.com>
2025-06-26 16:58:57 -0500

    (version) updated version

--- .eslint/rules/dependency-checks.mjs
+++ .eslint/rules/dependency-checks.mjs
@@ -10,7 +10,7 @@ export async function createDependencyChecksRule(ignoredDependencies) {
     files: ['./package.json'],
     rules: {
       '@nx/dependency-checks': [
-        'error',
+        'warn',
         {
           ignoredFiles: [
             '{projectRoot}/eslint.config.{js,cjs,mjs}',
