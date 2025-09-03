d116b68f604846293913591cbc9f3a732713c499
Robert Brightline <robert.brightline@gmail.com>
2025-09-02 21:27:15 -0500

    feat: created initial project template

--- apps/inventory/src/main.ts
+++ apps/inventory/src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app/app.module';
 import helmet from 'helmet';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
 
   const config = app.get(ConfigService);
@@ -14,12 +16,13 @@ async function bootstrap() {
   app.enableCors({ origin: ['*'] });
   app.setGlobalPrefix(PREFIX);
   app.use(helmet());
-
+  
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Inventory api')
     .addBearerAuth()
     .build();
   const swaggerDoc = SwaggerModule.createDocument(app, swaggerConfig, {});
+
   SwaggerModule.setup('api', app, swaggerDoc);
 
   await app.listen(PORT);
