408ea7a7a39d4aa2e4706e3439c4103c0fc89299
Ahmet Emrebas <aemrebasus@gmail.com>
2026-08-07 14:07:14 -0500

    feat: fixed the build configuration

--- libs/fs/src/index.ts
+++ libs/fs/src/index.ts
@@ -1,4 +1,4 @@
 // @index(['./**/*.ts', '!./**/*.spec.ts'], f => `export * from '${f.path}.js'`)
-export * from './read-json-file.js';
-export * from './read-text-file.js';
+export * from './lib/read-json-file.js';
+export * from './lib/read-text-file.js';
 
